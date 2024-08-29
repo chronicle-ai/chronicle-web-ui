cfe8e7c7af547e7860668f2a6fd66c2a50bb3bd5
freyes32 <freyes32@gatech.edu>
2024-08-29 02:56:21 -0400

    init commit

--- src/config/nav.js
+++ src/config/nav.js
@@ -4,14 +4,6 @@ export const nav = [
 		slug: '/',
 	},
 	{
-		title: 'Blog',
-		slug: '/blog',
-	},
-	{
-		title: 'About',
-		slug: '/company/about',
-	},
-	{
 		title: 'Contact',
 		slug: '/company/contact',
 	},
